306d243e5630482d3df9eccceac2a9348e268b1c
Rashid Bawah <rashbawah@gmail.com>
2025-09-12 04:10:34 -0600

    add code to script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,39 @@
-#!/usr/bin/bash
+# !/usr/bin/bash
+# !/usr/bin/python3
+
+# echo "Hello, World"
+# print("hello from python")
+
+# passing args to script
+# echo "Hello, $1!"
+# echo "Welcome!"
+# echo "Your age is: $2"
+# echo "Total args passed: $#"
+# echo "All args: $@"
+
+# echo
+
+# single, double-quotes, backticks shell within a shell
+echo 'Datetime: `date`'
+echo "Datetime: `date`"
+echo "Datetime: $(date)"
+
+echo 
+
+# shell-in-shell, continued
+date_time=$(echo $(date) | cut -d " " -f 2,3)
+echo "Todays date is: $date_time"
+
+# numbers in shell
+expr 5 + 3
+echo $((5 + 3))
+echo "10/3" | bc
+echo "scale=2; 10/3" | bc
 
 # declare -a first_array
 # first_array+=(1 2 3 4)
 # echo "First array: ${first_array[@]}"
+# echo "First array: ${first_array[*]}"
 # echo "Length of first array: ${#first_array[@]}"
 # echo "First element: ${first_array[0]}"
 
@@ -24,27 +55,28 @@
 # echo "Third array: ${third_array[@]}"
 # echo "slice: ${third_array[@]:1:2}"
 
-# declare -A city_details
-# city_details=([name]="Calgary" [population]=1600000 [province]="AB")
-# echo ${city_details[@]}
-# echo "Keys: ${!city_details[@]}" # return all keys
+# echo
+
+# associative arrays
+declare -A city_details
+city_details=([name]="Calgary" [population]=1600000 [province]="AB")
+echo ${city_details[@]}
+echo "Keys: ${!city_details[@]}" # return all keys
 
 # echo
 
-# declare -A city_details2=([name]="Vancouver" [population]=3000000 [province]="BC")
-# echo ${city_details2[@]}
+declare -A city_details2=([name]="Vancouver" [population]=3000000 [province]="BC")
 
+all_cities=(city_details city_details2)
 
-# all_cities=(city_details city_details2)
+for city_name in "${all_cities[@]}"; do
+    declare -n city="$city_name"
 
-# for city_name in "${all_cities[@]}"; do
-#     declare -n city="$city_name"
-
-#     echo "City: ${city[name]}"
-#     echo "Population: ${city[population]}"
-#     echo "Province: ${city[province]}"
-#     echo "---"
-# done
+    echo "City: ${city[name]}"
+    echo "Population: ${city[population]}"
+    echo "Province: ${city[province]}"
+    echo "---"
+done
 
 
 
@@ -105,9 +137,9 @@
 # print_hello # calling the function
 
 
-function print_filename {
-    local first_filename=$1
-    echo "Inside: $first_filename"
-}
-print_filename "TwoTowers.txt" "LOTR.txt"
-echo "Outside: $first_filename"
+# function print_filename {
+#     local first_filename=$1
+#     echo "Inside: $first_filename"
+# }
+# print_filename "TwoTowers.txt" "LOTR.txt"
+# echo "Outside: $first_filename"
